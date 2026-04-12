118074542e72adcfb9670768d670cf996fe2b367
colaziest2-ops <colaziest2@gmail.com>
2026-04-12 21:04:31 +0200

    Initial commit: Myaluza Civils website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001F3F',
+        orange: '#FF851B',
+      },
+    },
+  },
+  plugins: [],
+};
